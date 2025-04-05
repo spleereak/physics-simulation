294ca65f1c33072f22bd6d885bb3f089f17cb542
spleereak <spleereak@gmail.com>
2025-04-05 08:27:26 +0300

    add ValueRow component

---
+++ src/components/ValueRow.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  label: string;
+  value: number;
+  unit?: string;
+}
+
+export const ValueRow: React.FC<Props> = ({ label, value, unit='' }) => {
+  return (
+    <div className='flex justify-between items-center py-1.5 border-b border-gray-100 last:border-0'>
+      <span className='text-gray-700'>{label}:</span>
+      <span className='font-medium text-gray-900'>{value}{unit}</span>
+    </div>
+  )
+}
