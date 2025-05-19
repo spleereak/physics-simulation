d1329283c4a9bc1fb17a8cc005ed8b20a083184e
spleereak <spleereak@gmail.com>
2025-05-20 01:40:18 +0300

    update videos

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,13 +4,14 @@ export const mockVideos: Video[] = [
   {
     id: '1',
     title: 'Основы силы трения',
-    link: '/videos/youtube_JjFmIiccmis_1920x1080_h264.mp4',
-    duration: '6:30'
+    link: '/videos/youtube_ZbZwTC0Z1Os_1920x1080_h264.mp4',
+    duration: '14:51'
   },
   {
     id: '2',
     title: 'Сила трения в механике',
-    link: '/videos/youtube_ZbZwTC0Z1Os_1920x1080_h264.mp4',
-    duration: '14:51'
-  }
+    link: '/videos/youtube_JjFmIiccmis_1920x1080_h264.mp4',
+    duration: '6:30'
+  },
+
 ];
